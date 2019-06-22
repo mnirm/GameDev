9fbd02a6456424d6956e59b84be46b0f8c5043ca
jarnomehj <coolsjarno@gmail.com>
2019-06-22 15:03:37 +0200

    Added Sprite class, with code. Still have to test this

---
+++ PlatformerJarno/PlatformerJarno/Sprites/Sprite.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PlatformerJarno.Sprites
+{
+    // Sprite Class, Class for drawing sprites on screen
+    class Sprite
+    {
+        // Properties
+        public Texture2D Texture { get; }
+        public SpriteEffects SpriteEffect { get; set; }
+        public Vector2 Position { get; set; }
+        public Rectangle ViewRectangle { get; set; }
+        public float Scale { get; set; }
+
+        // Constructor
+        public Sprite(ContentManager content, string path, Vector2 startPosition, SpriteEffects spriteEffect = SpriteEffects.None, float scale = 1)
+        {
+            Texture = content.Load<Texture2D>(path);
+            Position = startPosition;
+
+            ViewRectangle = new Rectangle(0,0, Texture.Width, Texture.Height);
+            SpriteEffect = spriteEffect;
+            Scale = scale;
+        }
+
+        // Methods
+
+        // Draw Method, Draws the sprite on the screen with different properties.
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, ViewRectangle, Color.White, 0f, Vector2.Zero, Scale, SpriteEffect, 0f);
+        }
+    }
+}
