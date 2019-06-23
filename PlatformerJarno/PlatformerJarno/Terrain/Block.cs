a14e2ddc91ac3e5d17fcb52b2549a3bc7a1d8309
jarnomehj <coolsjarno@gmail.com>
2019-06-23 20:34:18 +0200

    Added Block Class and image Added empty collision class

---
+++ PlatformerJarno/PlatformerJarno/Terrain/Block.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using PlatformerJarno.Collider;
+using PlatformerJarno.Sprites;
+
+namespace PlatformerJarno.Terrain
+{
+    class Block : ICollision
+    {
+        // Properties
+        protected Sprite sprite;
+        protected Vector2 position;
+        public Rectangle CollisionRectangle
+        {
+            get
+            {
+                return new Rectangle(
+                    (int)position.X,
+                    (int)position.Y,
+                    (int)(sprite.ViewRectangle.Width * sprite.Scale),
+                    (int)(sprite.ViewRectangle.Height * sprite.Scale)
+                );
+            }
+        }
+
+        // Constructor
+        public Block(ContentManager content, string path, Vector2 position)
+        {
+            this.position = position;
+            sprite = new Sprite(content, path, position);
+            
+        }
+
+        // Methods
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch, position: position);
+        }
+    }
+}
