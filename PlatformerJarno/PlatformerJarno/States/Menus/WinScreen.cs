43f847677ce38b7f315a3ea22b3eff9b03da6cf2
jarnomehj <coolsjarno@gmail.com>
2019-06-27 15:57:52 +0200

    Made class WinScreen

---
+++ PlatformerJarno/PlatformerJarno/States/Menus/WinScreen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PlatformerJarno.States.Menus
+{
+    class WinScreen: MenuScreen
+    {
+        // Properties
+
+        // Constructor
+        public WinScreen(GraphicsDevice graphicsDevice) : base(graphicsDevice)
+        {
+        }
+
+        // Methods
+        public override void DrawMenuItems(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public override void UpdateMenuItems(GameTime gameTime)
+        {
+
+        }
+    }
+}
