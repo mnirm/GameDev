64d83c4548993870725adcda8cad90a47ce52760
jarnomehj <coolsjarno@gmail.com>
2019-07-01 23:15:52 +0200

    Added BreakableBlock and EntityHealth class EntityHealth is for refactoring Health and breakableblock is to make a block that is breakable by shooting it

---
+++ PlatformerJarno/PlatformerJarno/Terrain/BreakableBlock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using PlatformerJarno.Utilities;
+
+namespace PlatformerJarno.Terrain
+{
+    class BreakableBlock : Block
+    {
+        // Properties
+        private Health _health;
+
+        // Constructor
+        public BreakableBlock(ContentManager content, string path, Vector2 position, int health) : base(content, path, position)
+        {
+            // _health = new Health(health, content,);
+        }
+
+        // Methods
+
+    }
+}
---
+++ PlatformerJarno/PlatformerJarno/Utilities/EntityHealth.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using PlatformerJarno.Entities;
+
+namespace PlatformerJarno.Utilities
+{
+    class EntityHealth : Health
+    {
+        // Properties
+
+        // Constructor
+        public EntityHealth(int health, ContentManager content, Entity entity, float scale = 1) : base(health, content, entity, scale)
+        {
+        }
+
+        // Methods
+    }
+}
